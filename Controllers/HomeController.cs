863cba2f77f719da0ddbfa981d565521544276b6
Ірина Наконешнюк <nakonirina1996@gmail.com>
2023-08-14 23:44:27 +0300

    added helpers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Razor_PartialView.Models;
+using Razor_PartialView.Services;
 
 namespace Razor_PartialView.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IGetListsUserInfo _listsUserInfo;
+
+        public HomeController(IGetListsUserInfo listsUserInfo)
+        {
+            _listsUserInfo = listsUserInfo;
+        }
         public IActionResult MyData()
         {
+            ViewBag.Hobbies = _listsUserInfo.GetListHobbies();
+            ViewBag.Cities = _listsUserInfo.GetSelectedCity(); 
             return View();
         }
 
